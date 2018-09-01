50be4a769152f76dfff59d8bf0505585d2d470ab
svkreml <svkreml@gmail.com>
2018-09-01 23:39:00 +0300

    finally quit

--- src/main/java/svkreml/fun/sudoku/Solver.java
+++ src/main/java/svkreml/fun/sudoku/Solver.java
@@ -4,25 +4,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Iterator;
 import java.util.List;
 
 public class Solver {
     public static void main(String[] args) throws InterruptedException {
-        MyWebDriver myWebDriver = new MyWebDriver();
+        MyWebDriver myWebDriver = null;
+        try {
+            myWebDriver = new MyWebDriver();
 
 
-        myWebDriver.get("https://www.websudoku.com/");
-        Thread.sleep(1000);
-        WebDriver frame = myWebDriver.chromeDriver.switchTo().frame(0);
-        for (int i = 0; i < 10; i++) {
-            loop(myWebDriver);
+            myWebDriver.get("https://www.websudoku.com/");
+            Thread.sleep(1000);
+            WebDriver frame = myWebDriver.chromeDriver.switchTo().frame(0);
+            for (int i = 0; i < 10; i++) {
+                loop(myWebDriver);
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            myWebDriver.chromeDriver.quit();
         }
-
-
-        myWebDriver.chromeDriver.quit();
     }
 
     private static void loop(MyWebDriver myWebDriver) throws InterruptedException {
@@ -54,17 +58,23 @@ public class Solver {
                     WebElement webElement = iterator2.next();
                     String value = webElement.getAttribute("value");
                     if (!value.isEmpty()) continue;
-                    webElement.sendKeys(sodokuSolver.getCell(x,y).toString());
+                    webElement.sendKeys(sodokuSolver.getCell(x, y).toString());
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
+        try {
         Thread.sleep(2000);
         myWebDriver.wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@value=' How am I doing? ']")));
         myWebDriver.chromeDriver.findElement(By.xpath("//input[@value=' How am I doing? ']")).click();
         Thread.sleep(2000);
-        myWebDriver.wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@value=' Bring on a new puzzle! ']")));
-        myWebDriver.chromeDriver.findElement(By.xpath("//input[@value=' Bring on a new puzzle! ']")).click();
+
+            myWebDriver.wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@value=' Bring on a new puzzle! ']")));
+            myWebDriver.chromeDriver.findElement(By.xpath("//input[@value=' Bring on a new puzzle! ']")).click();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("решение проблемы плохой загрузки не предусмотрено");
+        }
     }
 }
